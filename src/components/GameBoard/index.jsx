87999ed27afe5d41847d81a0661f8ebc74323f42
zubair10P <zubair.shafi@10pearls.com>
2024-02-17 14:55:18 +0500

    feat: Player one symbol `X` added now

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { initialGameBoard } from "../../utils";
 
 const GameBoard = () => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectedSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      const udatedGameBoard = [
+        ...prevGameBoard.map((innerAray) => [...innerAray]),
+      ];
+      udatedGameBoard[rowIndex][colIndex] = "X";
+      return udatedGameBoard;
+    });
+  };
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handleSelectedSquare(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
